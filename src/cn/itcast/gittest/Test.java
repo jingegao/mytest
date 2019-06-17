685cea5dd72bb2340dea5b2dab1fbf812d3d79f0
jingegao <jinge_gao@163.com>
2019-06-17 09:59:23 +0800

    测试

---
+++ src/cn/itcast/gittest/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast.gittest;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("bbbbb");
+        System.out.println("ccccc");
+        System.out.println("ddddd");
+        System.out.println("eeeee");
+        System.out.println("fffff");
+    }
+}
